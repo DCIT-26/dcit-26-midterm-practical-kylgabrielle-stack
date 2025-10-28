2c9b0b6063973c8ba302c8f1f82177d4a499c8cb
kylgabrielle-stack <gabsykyl@gmail.com>
2025-10-28 18:19:09 +0800

    Resolved conflict.

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-print(“DCIT 26 – Midterm Practical Laboratory”)
-<<<<<<< HEAD
-print(“Submitted by Roflo, Kyl Gabrielle”)
-=======
-print(“Submitted by Roflo, Kyl Gabrielle”)
+print("DCIT 26 – Midterm Practical Laboratory")
+print("Submitted by Roflo, Kyl Gabrielle")
 print("Date of Submission: October 28, 2025")
->>>>>>> kyl_gabrielle
+
